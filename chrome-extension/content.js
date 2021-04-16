3115b2e1483da61cf8068e874c6f139369c90a2a
Jason Rhodes <jason.matthew.rhodes@gmail.com>
2021-04-16 12:02:14 -0400

    Removed unnecessary debug console logs

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,15 +1,11 @@
 window.addEventListener("load", function () {
   waitForElement("#top-level-buttons", 250, function (videoButtons) {
-    console.log("Found video buttons", videoButtons);
     var saveButton = videoButtons.querySelectorAll("a.yt-simple-endpoint")[3];
-    console.log("save button?", saveButton);
-
     saveButton.addEventListener("click", function () {
       waitForElement(
         "ytd-add-to-playlist-renderer #playlists",
         250,
         function (playlists) {
-          console.log("found playlists", playlists);
           var lists = playlists.querySelectorAll(
             "ytd-playlist-add-to-option-renderer"
           );
